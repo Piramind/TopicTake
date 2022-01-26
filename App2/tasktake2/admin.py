be42d5e5a63899f8bbecbb6de2c9f5ea16ba419e
Lev <tikhonov.lev0@gmail.com>
2022-01-26 13:17:53 +0400

    update admin

--- App2/tasktake2/admin.py
+++ App2/tasktake2/admin.py
@@ -4,8 +4,28 @@ from .models import StudyGroup, User, Discipline, Topic, Lesson, StudentOnTheLes
 
 admin.site.register(StudyGroup)
 admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('user_first_name', 'user_last_name', 'group_name')
+    fieldsets = (
+        (None, {
+            'fields': ('','', 'id')
+        }),
+        ('', {
+            'fields': ('', '')
+        }),
+    )
 admin.site.register(Discipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    list_filter = ('', '')
+
 admin.site.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_filter = ('topic_name', 'topic_user', 'topic_group')
+    
 admin.site.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_filter = ('', '')
+    
 admin.site.register(StudentOnTheLesson)
+
 # Register your models here.
